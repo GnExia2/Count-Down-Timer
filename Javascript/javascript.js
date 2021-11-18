f9a657dd4fde717b47766f056f0c839a9a166fb1
Garrick Ngai <garrickdark@yahoo.com>
2021-11-17 22:42:22 -0800

    re-factored a few lines and removed hours

--- Javascript/javascript.js
+++ Javascript/javascript.js
@@ -3,12 +3,10 @@ console.log("hello google team!");
 //Define vars to hold time values
 let seconds = 0;
 let minutes = 0;
-let hours = 0;
 
 //variables to hold display
 let displaySeconds = 0;
 let displayMinutes = 0;
-let displayHours = 0;
 
 
 //variable to hold setInterval
@@ -19,50 +17,39 @@ let status = "stopped";
 
 //countdown logic
 function stopWatch (){
-  seconds++
+    seconds++
 
 
-  //logic to determine next value;
-  if (seconds / 60 === 1) {
-    seconds = 0
-    minutes++
-
-    if (minutes / 60 === 1) {
-      minutes = 0
-      hours++
+    //logic to determine next value;
+    if (seconds / 60 === 1) {
+      seconds = 0
+      minutes++
     }
-  }
 
-  // when seconds/minutes/hours are only 1 digit, add a leading value of 0
-  if (seconds < 10) {
-    displaySeconds = "0" + seconds.toString();
-  }
-  else {
-    displaySeconds = seconds;
-  }
+    // when seconds/minutes/hours are only 1 digit, add a leading value of 0
+    if (seconds < 10) {
+      displaySeconds = "0" + seconds.toString();
+    }
+    else {
+      displaySeconds = seconds;
+    }
 
-  if (minutes < 10) {
-    displayMinutes = "0" + minutes.toString();
-  }
-  else {
-    displayMinutes = minutes;
-  }
-
-  if (hours < 10) {
-    displayHours = "0" + hours.toString();
-  }
-  else {
-    displayHours = hours;
-  }
+    if (minutes < 10) {
+      displayMinutes = "0" + minutes.toString();
+    }
+    else {
+      displayMinutes = minutes;
+    }
 
 
-  //Display updated time
-  document.getElementById("clockDisplay").innerHTML = displayHours + ":" + displayMinutes + ":" + displaySeconds;
-
+    document.getElementById("clockDisplay").innerHTML = displayMinutes + ":" + displaySeconds;
 }
 
 
-//function for button startStop
+  //Display updated time
+
+
+//function that is tied to start button that will change the text to stop when started and start when stopped
 function startStop (){
   if (status === "stopped") {
 
@@ -80,6 +67,14 @@ function startStop (){
   }
 }
 
+//function that is tied to reset button to reset the watch
+function reset(){
+  window.clearInterval(interval);
+  seconds = 0;
+  minutes = 0;
+  document.getElementById("clockDisplay").innerHTML = "00:00";
+  document.getElementById("startStop").innerHTML = "Start";
+}
 
 
 
