3b9c3c9605c40563299b85a13203866761c24b8d
Garrick Ngai <garrickdark@yahoo.com>
2021-11-17 19:49:00 -0800

    added functionality for start/stop to be controlled by button

--- Javascript/javascript.js
+++ Javascript/javascript.js
@@ -1,15 +1,21 @@
 console.log("hello google team!");
 
 //Define vars to hold time values
-let seconds = 0
-let minutes = 0
-let hours = 0
+let seconds = 0;
+let minutes = 0;
+let hours = 0;
 
 //variables to hold display
-let displaySeconds = 0
-let displayMinutes = 0
-let displayHours = 0
+let displaySeconds = 0;
+let displayMinutes = 0;
+let displayHours = 0;
 
+
+//variable to hold setInterval
+let interval = null;
+
+//defined variable to hold stopwatch status
+let status = "stopped";
 
 //countdown logic
 function stopWatch (){
@@ -27,10 +33,55 @@ function stopWatch (){
     }
   }
 
+  // when seconds/minutes/hours are only 1 digit, add a leading value of 0
+  if (seconds < 10) {
+    displaySeconds = "0" + seconds.toString();
+  }
+  else {
+    displaySeconds = seconds;
+  }
+
+  if (minutes < 10) {
+    displayMinutes = "0" + minutes.toString();
+  }
+  else {
+    displayMinutes = minutes;
+  }
+
+  if (hours < 10) {
+    displayHours = "0" + hours.toString();
+  }
+  else {
+    displayHours = hours;
+  }
+
+
   //Display updated time
-  document.getElementById("clockDisplay").innerHTML = hours + ":" + minutes + ":" + seconds;
+  document.getElementById("clockDisplay").innerHTML = displayHours + ":" + displayMinutes + ":" + displaySeconds;
 
 }
 
 
-window.setInterval(stopWatch, 1000)
+//function for button startStop
+function startStop (){
+  if (status === "stopped") {
+
+    //start stopwatch when status is set to start
+    interval = window.setInterval(stopWatch, 1000)
+    document.getElementById("startStop").innerHTML = "Stop"
+    status = "started"
+
+  }
+  else {
+    window.clearInterval(interval);
+    document.getElementById("startStop").innerHTML = "Start"
+    status = "stopped"
+
+  }
+}
+
+
+
+
+
+//Function to reset the stopWatch
